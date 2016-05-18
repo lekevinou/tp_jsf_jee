45f0185cc82f2ba57c02b69fe0b992f11f4cc95d
Thomas <thomas.soub@gmail.com>
2016-05-18 23:45:04 +0200

    ajout facelate et controleur

---
+++ src/main/java/subscription/SubsciptionController.java
@@ -0,0 +1,10 @@
+package subscription;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class SubsciptionController implements java.io.Serializable{
+
+}
