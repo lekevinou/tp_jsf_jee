71a0d3766aa1dabd55a2b1948134e34a13c15ce7
Thomas <thomas.soub@gmail.com>
2016-05-19 00:04:39 +0200

    ajout classes subscription et autres (repo, invalid...)

---
+++ src/main/java/subscription/Subscription.java
@@ -0,0 +1,71 @@
+package subscription;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Subscription implements java.io.Serializable{
+	private String email;
+	private String password;
+	private URL website;
+	private Date creationDate;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean checkPassword(String password) {
+		return this.password != null && password != null && ! password.isEmpty() && this.password.equals(password);
+	}
+	
+	public URL getWebsite() {
+		return website;
+	}
+
+	public void setWebsite(URL siteWeb) {
+		this.website = siteWeb;
+	}
+	
+	public boolean isWebsiteAvailable() {
+		if (this.website == null) {
+			return false;
+		}
+		try(InputStream s = this.website.openStream()) {
+			return true;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public boolean checkEmail() {
+		return Objects.toString(email, "").matches("\\S+@\\S+");
+	}
+
+	public boolean checkPassword() {
+		return Objects.toString(password, "").length() >= 8;
+	}
+
+}
