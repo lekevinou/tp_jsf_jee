9126cc334db9aa0f9693506cf7bd29c532486e94
Kevin <kevin.basse@epsi.fr>
2016-05-19 11:46:32 +0200

    Refacto, projet qui fonctoinne

--- src/main/java/subscription/InvalidSubscriptionException.java
+++ src/main/java/subscription/InvalidSubscriptionException.java
@@ -1,27 +1,27 @@
-package subscription;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
-
-@Named
-@RequestScoped
-public class InvalidSubscriptionException extends Exception implements java.io.Serializable{
-	
-	private static final long serialVersionUID = 1L;
-
-	private final List<String> errors = new ArrayList<>();
-	
-	public InvalidSubscriptionException() {
-	}
-	
-	public void addError(String message) {
-		errors.add(message);
-	}
-	
-	public List<String> getErrors() {
-		return errors;
-	}
-}
+//package subscription;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import javax.enterprise.context.RequestScoped;
+//import javax.inject.Named;
+//
+//@Named
+//@RequestScoped
+//public class InvalidSubscriptionException extends Exception implements java.io.Serializable{
+//	
+//	private static final long serialVersionUID = 1L;
+//
+//	private final List<String> errors = new ArrayList<>();
+//	
+//	public InvalidSubscriptionException() {
+//	}
+//	
+//	public void addError(String message) {
+//		errors.add(message);
+//	}
+//	
+//	public List<String> getErrors() {
+//		return errors;
+//	}
+//}
